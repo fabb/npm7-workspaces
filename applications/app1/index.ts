7e4799e6d0f80dd0138fe3e2b7384858b3621abc
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2020-10-06 10:35:11 +0200

    added a few workspaces

---
+++ applications/app1/index.ts
@@ -0,0 +1,6 @@
+import { c as c1 } from '@fabb/common1/common1'
+import { c as c2 } from '@fabb/common2/common2'
+
+export const app1 = () => {
+    return c1 + c2
+}
---
+++ applications/app2/index.ts
@@ -0,0 +1,5 @@
+import { c as c1 } from '@fabb/common1/common1'
+
+export const app1 = () => {
+    return c1
+}
